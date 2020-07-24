878af7c39a9a8ef3afb796409617916ef9a6a473
thomascheng <562985464@qq.com>
2020-07-24 10:23:11 +0800

    增加EnumMap类测试

--- src/com/dayup/base/MapTester.java
+++ src/com/dayup/base/MapTester.java
@@ -101,10 +101,64 @@ public class MapTester {
         System.out.println(table);
     }
 
+    enum Color {
+        RED,BLACK,BLUE
+    }
+
+    class Clothes {
+        String id;
+        Color color;
+        Clothes(String id, Color color) {
+            this.id = id;
+            this.color = color;
+        }
+    }
+
+    public void testEnumMap() {
+        List<Clothes> list = new ArrayList<>(8);
+        list.add(new Clothes("001", Color.BLACK));
+        list.add(new Clothes("002", Color.BLACK));
+        list.add(new Clothes("003", Color.RED));
+        list.add(new Clothes("004", Color.BLACK));
+        list.add(new Clothes("005", Color.BLACK));
+        list.add(new Clothes("006", Color.BLUE));
+        list.add(new Clothes("007", Color.BLUE));
+        list.add(new Clothes("008", Color.BLACK));
+        // use hashmap
+        Map<Color, Integer> map = new HashMap<>();
+        for (Clothes clothes : list) {
+            Color color = clothes.color;
+            Integer integer = map.get(color);
+            if (integer == null) {
+                map.put(color, 1);
+                map.put(null, 2); // 允许key为null
+            } else {
+                map.put(color, integer + 1);
+            }
+        }
+        System.out.println(map.toString());
+        // use EnumMap
+        // 应用两种map都可以实现统计，enummap底层使用了数组，效率更高，在key值是固定可枚举时，采用enummap。
+        Map<Color, Integer> map2 = new EnumMap<>(Color.class);
+        for (Clothes clothes : list) {
+            Color color = clothes.color;
+            Integer integer = map2.get(color);
+            if (integer == null) {
+                map2.put(color, 1);
+                // map2.put(null, 2); //不允许key为null，运行时typeCheck报空指针异常
+            } else {
+                map2.put(color, integer + 1);
+            }
+        }
+        // map中key的顺序与enum定义顺序一致
+        System.out.println(map2.toString());
+    }
+
     public static void test() {
         //new MapTester().testHashMap();
         //new MapTester().testLinkedHashMap();
         //new MapTester().testTreeMap();
-        new MapTester().testHashTable();
+        //new MapTester().testHashTable();
+        new MapTester().testEnumMap();
     }
 }
